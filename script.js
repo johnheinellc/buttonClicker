f379984a21aebee039f3369fbdc022e46be813da
johnheinellc <johnheinellc@gmail.com>
2022-02-01 12:45:09 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// Login/Logout button
+function changeText(element){
+    element.innerText = "Logout";
+}
+
+// definition button disappears
+function disappear(element){
+    element.style.visibility = "hidden";
+}
+
+// alert like button was not needed in .js
+
+// counter
+// var like1=document.getElementById(like1);
+// console.log(like1);
+// var counter=0
+// like1.innerText = counter
+// function increments(){
+//     counter++
+//     like1.innerText = counter
+// }
